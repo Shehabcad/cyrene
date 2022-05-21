f2b674470af1971da9574bd9379c66e7b80f6484
Shehabcad <42914908+Shehabcad@users.noreply.github.com>
2022-05-21 17:57:51 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1E8hcA-LQJ0ZG3-fHHboXVtTHyR6LOI3yYXKAwpe5qho/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCgZ8uwleW-qonx9rE8U6F_1NPLTEEMNJ8';
